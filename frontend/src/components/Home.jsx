5f57346ce9642ce7fc92101ff50c7957bf81dbb7
amritRaj789 <amritrazzz@gmail.com>
2026-01-29 16:48:17 +0530

    deleted old components that are not required

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,7 +2,7 @@ import {Link} from 'react-router';
 
 const Home = ({user, error}) => {
     return(
-        <div className="home-style min-h-screen flex-column items-center justify-center">
+        <div className="home-style min-h-screen flex-column ">
             <div className="mb-8">
                 <h1>Welcome to Duckman Tech</h1>
             </div>
@@ -10,9 +10,12 @@ const Home = ({user, error}) => {
             <div className="bg-white p-6 rounded-lg shadow-lg w-full text-center">
                 {error && <h1 className="text-red-500">{error}</h1>}
                 {user ? 
-                (<div >
+                (<div className='justify-items-center'>
                     <h2 className="font-bold mb-6 text-gray-900"> Hello, {user.username}</h2>
                     <p className="text-gray-400">You are logged in with Email Id: {user.email}</p>
+                    <div className='mt-8 rounded-lg bg-gray-200 w-sm'>
+                        <Link className='text-2xl text-white font-bold' to='/dashboard'>Checkout Dashboard</Link>
+                    </div>
                 </div> ) :
                 (<div>
                     <h2 className="text-lg font-bold mb-6 text-gray-900">User not logged in</h2>
