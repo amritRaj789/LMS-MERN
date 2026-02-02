28459c21a55ae18ce92c4db38d95c8fa9557877c
amritRaj789 <amritrazzz@gmail.com>
2026-02-02 19:23:50 +0530

    made dashboard tab clickable, redirects to dashboard

--- frontend/src/components/dashboard/Sidebar.jsx
+++ frontend/src/components/dashboard/Sidebar.jsx
@@ -4,10 +4,15 @@ const Sidebar = () => {
 
   let navigate = useNavigate();
 
-  const handleItemClick = () => {
+  const handleItemClick = (e) => {
+    const value = e.target.dataset.value;
     console.log("Clicked on Settings");
-    navigate('/dashboard/settings');
-
+    if(value == "settings"){
+      navigate('/dashboard/settings');
+    }
+    else if(value == "dashboard"){
+      navigate('/dashboard');
+    }
   };
 
   
@@ -17,12 +22,12 @@ const Sidebar = () => {
       <h2 className="logo">LMS</h2>
 
       <ul className="menu">
-        <li>Dashboard</li>
+        <li role="button" data-value="dashboard" onClick={handleItemClick}>Dashboard</li>
         <li>Courses</li>
         <li>Assignments</li>
         <li>Students</li>
 
-        <li role="button" onClick={handleItemClick}>Settings</li>
+        <li role="button" data-value="settings" onClick={handleItemClick}>Settings</li>
       </ul>
     </div>
   );
