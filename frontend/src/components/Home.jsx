60e2a283e2df835421aff60bb9d1e6bfe11ab577
amritRaj789 <amritrazzz@gmail.com>
2026-02-06 14:01:22 +0530

    removed change username from Home component

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,37 +1,7 @@
-import { useState } from 'react';
+
 import {Link} from 'react-router';
-import axios from "axios";
 
-const Home = ({user, setUser, error}) => {
-
-    const [showForm, setShowForm] = useState(false);
-    const [username, setUsername] = useState("");
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        console.log("New username:", username);
-        setShowForm(false); // hide form after submit
-
-        try {
-            // send a post request
-            console.log("the user data of logged in user: ", user);
-
-            if(user.id != undefined){
-                const res = await axios.post('/api/users/update', {username: username, id: user.id}); // username is new, id is old
-                // update only the username in frontend
-                setUser(prev => ({ ...prev, username: res.data.username}));
-            }
-            else{
-                const res = await axios.post('/api/users/update', {username: username, id: user._id}); // username is new, id is old
-                // update only the username in frontend
-                setUser(prev => ({ ...prev, username: res.data.username}));
-            }
-
-        } catch (error) {
-            // catch any error
-            console.log("Error in catch block: ", error);
-        }
-    };
+const Home = ({user, error}) => {
 
     return(
         <div className="home-style min-h-screen flex-column ">
@@ -45,24 +15,8 @@ const Home = ({user, setUser, error}) => {
                 (<div className='justify-items-center'>
                     <h2 className="font-bold mb-6 text-gray-900"> Hello, {user.username}</h2>
                     <p className="text-gray-400">You are logged in with Email Id: {user.email}</p>
-                    <div className='mt-8 rounded-lg bg-gray-200 w-sm'>
-                        <Link className='text-2xl text-white font-bold p-2 rounded hover:bg-gray-300' to='/dashboard'>Checkout Dashboard</Link>
-                        <button className='my-4 bg-gray-200 rounded hover:bg-gray-300' onClick={() => setShowForm(!showForm)}>
-                            Change Username
-                        </button>
-                        {showForm && (
-                            <form onSubmit={handleSubmit} style={{ marginTop: "10px", marginBottom: "30px"}}>
-                            <input
-                                className='text-gray-900 p-2'
-                                type="text"
-                                placeholder="Enter new username"
-                                value={username}
-                                onChange={(e) => setUsername(e.target.value)}
-                                required
-                            />
-                            <button type="submit">Save</button>
-                            </form>
-                        )}
+                    <div className='mt-8 rounded-lg  w-sm'>
+                        <Link className='text-xl text-white font-bold p-2 rounded hover:bg-gray-100' to='/dashboard'>Checkout Dashboard</Link>
                     </div>
                 </div> ) :
                 (<div>
